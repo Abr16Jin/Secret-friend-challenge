ce8bc904e0dd1f9d0accc9d8b18bbe90e39b82b6
Abr16Jin <axeltriple1603@gmail.com>
2025-08-05 21:04:29 -0600

    Funcion Agregar amigo implementada, funcion de textos HTML elements, funcion de limpieza en Input

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let amigosCantidad =0;
+
+function agregarAmigo()
+    {        
+        let amigo_input = document.getElementById("amigo").value.trim();
+        if(!isNaN(amigo_input))
+        {
+            alert("¡Escribe un nombre!")
+        
+         } 
+         else if(amigos.includes(amigo_input))
+            {
+              alert("¡Ese amigo ya existe!")
+        }
+         else if(amigo_input === '')
+            {
+                alert("¡Escribe un nombre!");
+        }
+        else{
+                amigos.push(amigo_input);
+                update_list();  
+                amigosCantidad++;             
+            }     
+        
+        limpiarCaja();
+    }
+function element_text(elemento, texto)  //solo eventos
+    {
+        let elementoHTML = document.querySelector(elemento);
+        elementoHTML.innerHTML = texto;
+        return;
+    }//funcion texts de HTML elements
+function limpiarCaja()
+    {
+        let texto_caja = document.querySelector('#amigo').value = '';
+    }// Clean boxes
